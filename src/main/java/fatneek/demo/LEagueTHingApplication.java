8b1fddbcc835b6922701e74845e72ec3d631eadd
jan <jan.schrewe@yahoo.de>
2022-01-21 16:26:16 +0100

    first commit

---
+++ src/main/java/fatneek/demo/LEagueTHingApplication.java
@@ -0,0 +1,13 @@
+package fatneek.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LEagueTHingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LEagueTHingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fatneek/demo/LEagueTHingApplicationTests.java
@@ -0,0 +1,13 @@
+package fatneek.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LEagueTHingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
